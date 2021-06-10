950895e4a1dbd10fe2d78e0cd5ba5535df37e7ba
Noah Krill <51893284+NoahKrill11@users.noreply.github.com>
2021-06-10 14:26:44 -0400

    Add validation and bug fixes

--- test.java
+++ test.java
@@ -12,35 +12,71 @@ private static String getHash(final String hashtype, final File fileName)
 {
 	int bc=0;
 	StringBuilder stringBuild = new StringBuilder();
-	try
+	if(fileName.exists())
 	{
-		//getting the stream of the file content
-	FileInputStream stream = new FileInputStream(fileName);
+		
+		try
+		{
+			//getting the stream of the file content
+		FileInputStream stream = new FileInputStream(fileName);
 
-	// spliting the data into chuncks ina byte array
-	byte[] byteArray = new byte[1024];
-	MessageDigest digest = MessageDigest.getInstance(hashtype);
-	while ((bc = stream.read(byteArray))!=-1)
-	{
-		digest.update(byteArray,0,bc);
+		// spliting the data into chuncks ina byte array
+		byte[] byteArray = new byte[1024];
+		MessageDigest digest = MessageDigest.getInstance(hashtype);
+		while ((bc = stream.read(byteArray))!=-1)
+		{
+			digest.update(byteArray,0,bc);
+		}
+		stream.close();
+
+		byte[] bytes = digest.digest();
+
+		//converting to hexadecimal
+		for(int i=0; i< bytes.length; ++i)
+		{
+			stringBuild.append(Integer.toString((bytes[i] & 0xff)+ 0x100, 16).substring(1));
+		}
+
+		}
+		catch(Exception e)
+		{
+			System.out.println("Something went wrong");
+		}
 	}
-	stream.close();
-
-	byte[] bytes = digest.digest();
-
-	//converting to hexadecimal
-	for(int i=0; i< bytes.length; ++i)
+	else
 	{
-		stringBuild.append(Integer.toString((bytes[i] & 0xff)+ 0x100, 16).substring(1));
-	}
 
-	}
-	catch(Exception e)
-	{
-		System.out.println("Something went wrong");
+		return "false";
+
 	}
 	return stringBuild.toString();
+}
+// keeps on doing this validate function until file is found.
+private static void validate()
+{
 
+	System.out.println("The file you entered does not exist or is not in the right directory try again ");
+	Scanner scan = new Scanner(System.in);
+	File file = new File(scan.nextLine());
+	output(file);
+}
+
+//function for all the outputs
+private static void output(final File file)
+{
+	String hash = getHash("Sha-256", file);
+		if(hash != "false")
+		{
+		System.out.println("The hash for Sha-256 is " + hash);
+		hash = getHash("Sha-1", file);
+		System.out.println("The hash for Sha-1 is " + hash);
+		hash = getHash("MD5", file);
+		System.out.println("The hash for MD5 is " + hash);
+	}
+	if(hash== "false")
+		{
+			validate();
+		}
 }
 //This is the driver code
 public static void main(String[] args)
@@ -48,11 +84,7 @@ public static void main(String[] args)
 		System.out.println("Name of file");
 		Scanner scan = new Scanner(System.in);
 		File file = new File(scan.nextLine());
-		String hash = getHash("Sha-256", file);
-		System.out.println("The hash for Sha-256 is " + hash);
-		hash = getHash("Sha-1", file);
-		System.out.println("The hash for Sha-1 is " + hash);
-		hash = getHash("MD5", file);
-		System.out.println("The hash for MD5 is " + hash);
+		output(file);
+		
 	}
 }
